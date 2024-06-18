2ef2a04420b7e4994d1d6253739f9d051bf03c9f
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-18 12:33:41 +0530

    popular

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -3,14 +3,15 @@ import { Link } from 'react-router-dom'
 
 const Item = ({id, name, image, old_price, new_price}) => {
   return (
-    <div>
-      <div>
+    <div className='rounded-xl overflow-hidden shadow-lg'>
+      <div className='relative flexCenter group overflow-hidden transition-all duration-100'>
         <Link to={`product/${id}`}></Link>
         <img src={image} alt="productImage" />
       </div>
       <div>
         <h4>{name}</h4>
         <div>
+            <div>{new_price}</div>
             <div>{old_price}</div>
         </div>
       </div>
