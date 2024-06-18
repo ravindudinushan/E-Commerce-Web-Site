3726c23576e5d0fbff130d099e596e7b87f3d689
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-18 12:46:32 +0530

    popular

--- frontend/src/components/Item.jsx
+++ frontend/src/components/Item.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import {FaSearch} from 'react-icons/fa'
 
 const Item = ({id, name, image, old_price, new_price}) => {
   return (
     <div className='rounded-xl overflow-hidden shadow-lg'>
       <div className='relative flexCenter group overflow-hidden transition-all duration-100'>
-        <Link to={`product/${id}`}></Link>
+        <Link to={`product/${id}`} className='h-12 w-12 bg-white rounded-full flexCenter absolute 
+        top-1/2 bottom-1/2 !py-2 z-20 transition-all duration-700'>
+            <FaSearch className='hover:rotate-90 hover:scale-125 transition-all duration-200'/>
+        </Link>
         <img src={image} alt="productImage" />
       </div>
       <div>
