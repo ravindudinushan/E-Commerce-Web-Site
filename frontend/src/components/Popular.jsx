6347324dd2dd337c998dac477b6f97ef7339bf2f
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-18 15:00:25 +0530

    popular

--- frontend/src/components/Popular.jsx
+++ frontend/src/components/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
     <section className='bg-primary'>
         <div className='max_padd_container py-12 xl:py-28 xl:w-[88%]'>
             <h3 className='h3 text-center'>Popular Products</h3>
-            <hr className='h-[3px] md:w-1/2 mx-auto bg-gradient-to-1 from-transparent via-black to-transparent mb-16'/>
+            <hr className='h-[3px] md:w-1/2 mx-auto bg-gradient-to-r from-transparent via-black to-transparent mb-16'/>
             {/* container */}
             <div className='grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6'>
                 {POPULAR.map((item) => (
