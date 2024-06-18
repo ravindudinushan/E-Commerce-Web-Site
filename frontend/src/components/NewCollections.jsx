8f0561db274153d8a858d9ef7eac3188f65005b4
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-18 19:20:35 +0530

    offer

---
+++ frontend/src/components/NewCollections.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewCollections = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewCollections
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Popular from '../components/Popular'
+import Offer from '../components/Offer'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <Popular/>
+      <Offer/>
     </>
   )
 }
