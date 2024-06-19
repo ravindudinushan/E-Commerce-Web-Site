58ed2d24599acf9357e0f9fece19b9ceae84a285
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-19 11:09:07 +0530

    product header

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import useParems from 'react-router-dom'
+import ProductHd from '../components/ProductHd';
 
 const Product = () => {
+  const {all_products} = useContext(ShopContext);
+  const {productId} = useParems();
+  const product = all_products.find((e) => e.id === Number(productId))
   return (
-    <div>
-      
-    </div>
+    <section>
+      <div>
+        <ProductHd/>
+      </div>
+    </section>
   )
 }
 
