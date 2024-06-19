ff0b34fa542a66de01eb0c8a93190681c5b87efc
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-19 10:13:31 +0530

    shopContext

--- frontend/src/pages/Category.jsx
+++ frontend/src/pages/Category.jsx
@@ -9,17 +9,20 @@ const Category = ({category, banner}) => {
         <div>
           <img src={banner} alt="" className='block my-7 mx-auto'/>
         </div>
-        <div>
-          <h5><span>Showing 1-12</span> out of 36 products</h5>
-          <div>Sort by <mdOutlineKeyboardArrowDown/></div>
+        <div className='flexBetween my-8 mx-2'>
+          <h5><span className='font-bold'>Showing 1-12</span> out of 36 products</h5>
+          <div className='flexBetween max-sm:p-4 gap-x-4 px-8 py-3 rounded-5xl ring-1 ring-slate-900/15'>Sort by <mdOutlineKeyboardArrowDown/></div>
         </div>
         {/* container */}
-        <div>
+        <div className='grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6'>
           {all_products.map((item) => {
-            if(category){
+            if(category === item.category){
               return <Item key={item.id} id={item.id} image={item.image} name={item.name} new_price={item.new_price} old_price={item.old_price}/>
             }
           })}
+        </div>
+        <div className='mt-16 text-center'>
+          <button className='btn_dark_rounded'>Load more</button>
         </div>
       </div>
     </section>
