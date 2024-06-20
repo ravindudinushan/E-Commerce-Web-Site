d19011de515f48bd0c15cffc9eddcc83a2bfd2e9
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-20 19:18:05 +0530

    Add to cart

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,10 +1,22 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import all_products from '../assets/all_products'
 
 export const ShopContext = createContext(null);
 
+const getDefaultCart = () => {
+    let cart = {};
+    for(let index = 0; index < all_products.length; index++){
+        cart[index] = 0;
+    }
+    return cart;
+}
+
 const ShopContextProvider = (props) => {
-    const contextValue = {all_products};
+
+const [cartItems, setCartItems] = useState(getDefaultCart());
+const contextValue = {all_products, cartItems};
+
+console.log(cartItems);
 
     return(
         <ShopContext.Provider value={contextValue}>
