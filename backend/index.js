a566453e9defbd3db623cfc11c66f420948c02c9
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-24 11:40:43 +0530

    config mongodb

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,24 @@ app.get("/", (req, res) => {
     res.send("Express App is running")
 })
 
+// Image storage engine
+const storage = multer.diskStorage({
+    destination: "./upload/images",
+    filename: (req, file, cb) => {
+        return cb(null, `${file,fieldname}_${data.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({storage:storage})
+// creating upload endpoint for images
+app.use('/images', express.static('upload/images'))
+app.post("/upload", upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
 app.listen(port, (error) => {
     if(!error){
         console.log("Server is running on port " + port);
