3a4eb1d9020d7e416a2c89bfeb4810a82dcdad62
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-24 17:16:16 +0530

    image upload

--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,60 @@ app.post("/upload", upload.single('product'), (req, res) => {
     })
 })
 
+//schema for creating product
+const product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    }, 
+    category: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    available: {
+        type: Boolean,
+        default: true,
+    },
+})
+
+app.post('/addproduct', async(req, res) => {
+    const product = new Product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
+})
+
 app.listen(port, (error) => {
     if(!error){
         console.log("Server is running on port " + port);
