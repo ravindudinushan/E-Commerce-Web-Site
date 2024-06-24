91bd6c9ea5a557992b0923aec762e72b28f5d3e9
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-24 11:14:20 +0530

    config mongodb

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,23 @@ const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const path = require("path");
 const cors = require("cors");
+const { error } = require("console");
 
 app.use(express.json());
 app.use(cors());
+
+// Database connection with mondodb
+mongoose.connect("mongodb+srv://ecommerce:user1234@cluster0.34qevlv.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0");
+
+// Api creation
+app.get("/", (req, res) => {
+    res.send("Express App is running")
+})
+
+app.listen(port, (error) => {
+    if(!error){
+        console.log("Server is running on port " + port);
+    }else{
+        console.log("Error : " + error);
+    }
+})
