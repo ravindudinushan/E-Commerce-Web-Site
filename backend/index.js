c1db2dce5b0452048e9abb4c4c2fd76741e8f90f
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-25 16:17:24 +0530

    save

--- backend/index.js
+++ backend/index.js
@@ -74,6 +74,16 @@ const Product = mongoose.model("Product", {
 })
 
 app.post('/addproduct', async(req, res) => {
+    let products = await Product.find({});
+    let id;
+    if(products.length > 0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id +1;
+    }else{
+        id = 1;
+    }
+
     const product = new Product({
         id: req.body.id,
         name: req.body.name,
