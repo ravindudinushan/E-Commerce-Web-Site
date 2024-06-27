5821b8774f144c6bff05705c3377ba55c392b033
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-27 17:17:35 +0530

    Sidebar

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import addProduct from '../assets/addproduct.png'
+import ListProduct from '../assets/productlist.png'
 
 const Sidebar = () => {
   return (
-    <div>
-      
+    <div className='py-7 flex justify-center gap-x-1 gap-y-5 w-full bg-white sm:gap-x-4 lg:flex-col 
+    lg:pt-20 lg:max-w-60 lg:h-screen lg:justify-start lg:pl-6'>
+      <Link to={'/addproduct'}>
+        <button className='flexCenter gap-2 rounded-md bg-primary h-14 w-44 medium-16'>
+            <img src={addProduct} alt="" height={55} width={55}/>
+            <span>Add Product</span>
+        </button>
+      </Link>
+      <Link to={'/listproduct'}>
+        <button className='flexCenter gap-2 rounded-md bg-primary h-14 w-44 medium-16'>
+            <img src={ListProduct} alt="" height={55} width={55}/>
+            <span>Product List</span>
+        </button>
+      </Link>
     </div>
   )
 }
--- admin/src/pages/Admin.jsx
+++ admin/src/pages/Admin.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Sidebar from '../components/Sidebar'
+import { Route, Routes } from 'react-router-dom'
+import AddProduct from '../components/AddProduct'
+import ListProduct from '../components/ListProduct'
 
 const Admin = () => {
   return (
-    <div>
+    <div className='lg:flex'>
       <Sidebar/>
+      <Routes>
+        <Route path='/addproduct' element={<AddProduct/>}/>
+        <Route path='/listproduct' element={<ListProduct/>}/>
+      </Routes>
     </div>
   )
 }
