aa841b6efd80ad69e080649c637fa2d6814f7fde
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-27 10:47:24 +0530

    admin navbar

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -4,8 +4,11 @@ import profileImg from '../assets/profile.png'
 
 const Navbar = () => {
   return (
-    <nav>
+    <nav className='max_padd_container flexBetween bg-white py-2 ring-1 ring-slate-900/5 relative '>
       <div><img src={logo} alt="" /></div>
+      <div className='uppercase bold-22 text-white bg-secondary px-3 rounded-md tracking-widest 
+      line-clamp-1 max-xs:bold-18 max-xs:py-2 max-xs:px-1'>Admin Panel</div>
+      <div><img src={profileImg} alt="" className='h-12 w-12 rounded-full'/></div>
     </nav>
   )
 }
