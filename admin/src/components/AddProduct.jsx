04b1dbfc9196c6258fe1a5a1215f503976e05f1e
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-27 17:46:43 +0530

    Sidebar

--- admin/src/components/AddProduct.jsx
+++ admin/src/components/AddProduct.jsx
@@ -3,7 +3,31 @@ import React from 'react'
 const AddProduct = () => {
   return (
     <div>
-      
+      <div>
+        <h4 className='bold-18 pb-2'>Product title:</h4>
+        <input type="text" name='name' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
+      </div>
+      <div>
+        <h4 className='bold-18 pb-2'>Price:</h4>
+        <input type="text" name='old_price' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
+      </div>
+      <div>
+        <h4 className='bold-18 pb-2'>Product title:</h4>
+        <input type="text" name='new_price' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
+      </div>
+      <div>
+        <h4 className='bold-18 pb-2'>Product Category:</h4>
+        <select name="category" id="" className='bg-primary'>
+            <option value="Women"></option>
+            <option value="Men"></option>
+            <option value="Kid"></option>
+        </select>
+      </div>
+      <div>
+        <label htmlFor="file-input">
+            <img src="" alt="" />
+        </label>
+      </div>
     </div>
   )
 }
--- admin/src/components/ListProduct.jsx
+++ admin/src/components/ListProduct.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ListProduct = () => {
   return (
     <div>
-      
+      list product
     </div>
   )
 }
