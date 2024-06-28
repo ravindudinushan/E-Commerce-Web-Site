a882fba5ac3768c766b4b095609edeb4ab5c3c67
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-28 10:21:29 +0530

    update

--- admin/src/components/AddProduct.jsx
+++ admin/src/components/AddProduct.jsx
@@ -3,8 +3,8 @@ import upload_area from '../assets/upload_area.svg'
 
 const AddProduct = () => {
   return (
-    <div>
-      <div>
+    <div className='p-8 box-border bg-white w-full rounded-sm mt-4 lg:m-7'>
+      <div className='mb-3'>
         <h4 className='bold-18 pb-2'>Product title:</h4>
         <input type="text" name='name' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
       </div>
@@ -16,18 +16,19 @@ const AddProduct = () => {
         <h4 className='bold-18 pb-2'>Product title:</h4>
         <input type="text" name='new_price' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
       </div>
-      <div>
+      <div className='mb-3 flex items-center gap-x-4'>
         <h4 className='bold-18 pb-2'>Product Category:</h4>
-        <select name="category" id="" className='bg-primary'>
-            <option value="Women"></option>
-            <option value="Men"></option>
-            <option value="Kid"></option>
+        <select name="category" id="" className='bg-primary ring-1 ring-slate-900/20 meduim-16 rounded-sm outline-none '>
+            <option value="women">Women</option>
+            <option value="men">Men</option>
+            <option value="kid">Kid</option>
         </select>
       </div>
       <div>
         <label htmlFor="file-input">
-            <img src={upload_area} alt="" />
+            <img src={upload_area} alt="" className='w-20 rounded-sm inline-block'/>
         </label>
+        <input type="file" name='image' id='file-input' hidden className='bg-primary max-w-80 w-full py-3 px-4'/>
       </div>
     </div>
   )
--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -8,14 +8,14 @@ const Sidebar = () => {
     <div className='py-7 flex justify-center gap-x-2 gap-y-5 w-full bg-white sm:gap-x-4 lg:flex-col 
     lg:pt-20 lg:max-w-60 lg:h-screen lg:justify-start lg:pl-6'>
       <Link to={'/addproduct'}>
-        <button className='flexCenter gap-2 rounded-md bg-primary h-14 w-40 xs:w-44 medium-16'>
-            <img src={addProduct} alt="" height={55} width={55}/>
+        <button className='flexCenter gap-2 rounded-md bg-primary h-12 w-40 xs:w-44 medium-16'>
+            <img src={addProduct} alt="" height={50} width={50}/>
             <span>Add Product</span>
         </button>
       </Link>
       <Link to={'/listproduct'}>
-        <button className='flexCenter gap-2 rounded-md bg-primary h-14 w-40 xs:w-44 medium-16'>
-            <img src={ListProduct} alt="" height={55} width={55}/>
+        <button className='flexCenter gap-2 rounded-md bg-primary h-12 w-40 xs:w-44 medium-16'>
+            <img src={ListProduct} alt="" height={50} width={50}/>
             <span>Product List</span>
         </button>
       </Link>
