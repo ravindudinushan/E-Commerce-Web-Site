c3fa960a25800d37097c01075114c8f86a1e93b2
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-29 21:56:41 +0530

    add product

--- admin/src/components/AddProduct.jsx
+++ admin/src/components/AddProduct.jsx
@@ -5,27 +5,43 @@ import {MdAdd} from 'react-icons/md'
 const AddProduct = () => {
 
   const [image, setImage] = useState(false);
+  const [productDetails, setProductDetails] = useState({
+    name: "",
+    image: "",
+    category: "women",
+    new_price: "",
+    old_price: ""
+  })
+
   const imageHandler = (e) => {
       setImage(e.target.files[0])
+  }
+
+  const changeHandler = (e) => {
+    setProductDetails({...productDetails, [e.target.name]:e.target.value})
+  }
+
+  const Add_Product = async () => {
+    console.log(productDetails);
   }
 
   return (
     <div className='p-8 box-border bg-white w-full rounded-sm mt-4 lg:m-7'>
       <div className='mb-3'>
         <h4 className='bold-18 pb-2'>Product title:</h4>
-        <input type="text" name='name' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
+        <input value={productDetails.name} onChange={changeHandler} type="text" name='name' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
       </div>
       <div>
         <h4 className='bold-18 pb-2'>Price:</h4>
-        <input type="text" name='old_price' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
+        <input value={productDetails.old_price} onChange={changeHandler} type="text" name='old_price' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
       </div>
       <div>
-        <h4 className='bold-18 pb-2'>Product title:</h4>
-        <input type="text" name='new_price' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
+        <h4 className='bold-18 pb-2'>Offer Price:</h4>
+        <input value={productDetails.new_price} onChange={changeHandler} type="text" name='new_price' placeholder='Type here..' className='bg-primary outline-none max-w-80 w-full py-3 px-4 rounded-md'/>
       </div>
       <div className='mb-3 flex items-center gap-x-4'>
         <h4 className='bold-18 pb-2'>Product Category:</h4>
-        <select name="category" id="" className='bg-primary ring-1 ring-slate-900/20 meduim-16 rounded-sm outline-none '>
+        <select value={productDetails.category} onChange={changeHandler} name="category" id="" className='bg-primary ring-1 ring-slate-900/20 meduim-16 rounded-sm outline-none '>
             <option value="women">Women</option>
             <option value="men">Men</option>
             <option value="kid">Kid</option>
@@ -37,7 +53,7 @@ const AddProduct = () => {
         </label>
         <input onChange={imageHandler} type="file" name='image' id='file-input' hidden className='bg-primary max-w-80 w-full py-3 px-4'/>
       </div>
-      <button className='btn_dark_rounded mt-4 flexCenter gap-x-1'><MdAdd/>Add Product</button>
+      <button onClick={() => Add_Product()} className='btn_dark_rounded mt-4 flexCenter gap-x-1'><MdAdd/>Add Product</button>
     </div>
   )
 }
