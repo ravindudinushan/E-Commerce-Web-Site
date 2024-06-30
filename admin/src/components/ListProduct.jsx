371d80dde9c3d4699e7d6db4944afc04628ac101
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-30 22:03:01 +0530

    Update

--- admin/src/components/ListProduct.jsx
+++ admin/src/components/ListProduct.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {TbTrash} from "react-icons/tb"
 
 const ListProduct = () => {
+
+  const [allProducts, setAllProducts] = useState([]);
+
+  const fetchInfo = async () => {
+    await fetch('http://localhost:4000/allproducts').then((res) => res.json()).then((data) => {
+      setAllProducts(data)
+    })
+  }
+
+  useEffect(() => {
+    fetchInfo();
+  }, [])
+
   return (
-    <div>
-      list product
+    <div className='p-2 box-border bg-white mb-0 rounded-sm w-full mt-4 sm:p-4 sm:m-7'>
+      <h4 className='bold-22 p-5 uppercase'>Product List</h4>
+      <div className='max-h-[77vh] overflow-auto px-4 text-center'>
+        <table className='w-full mx-auto'>
+          <thead>
+            <tr className='bg-primary bold-14 sm:regular-22 text-start py-12'>
+              <th className='p-2'>Products</th>
+              <th className='p-2'>Title</th>
+              <th className='p-2'>Old Price</th>
+              <th className='p-2'>New Price</th>
+              <th className='p-2'>Category</th>
+              <th className='p-2'>Remove</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allProducts.map((product, i) => (
+              <tr key={i}>
+                <td>
+                  <img src={product.image} alt="" />
+                </td>
+                <td><div>{product.name}</div></td>
+                <td>${product.old_price}</td>
+                <td>${product.new_price}</td>
+                <td>{product.category}</td>
+                <td><div><TbTrash/></div></td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
