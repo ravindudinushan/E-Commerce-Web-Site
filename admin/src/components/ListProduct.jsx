6ecdfdb41169cdf5b5a35a11c821cbf05c5a0325
ravindudinushan <ravindudinushan9966@gmail.com>
2024-06-30 22:33:20 +0530

    product list

--- admin/src/components/ListProduct.jsx
+++ admin/src/components/ListProduct.jsx
@@ -15,6 +15,17 @@ const ListProduct = () => {
     fetchInfo();
   }, [])
 
+  const remove_product = async (id) => {
+    await fetch('http://localhost:4000/removeproducts', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({id:id})
+    })
+  }
+
   return (
     <div className='p-2 box-border bg-white mb-0 rounded-sm w-full mt-4 sm:p-4 sm:m-7'>
       <h4 className='bold-22 p-5 uppercase'>Product List</h4>
@@ -32,15 +43,15 @@ const ListProduct = () => {
           </thead>
           <tbody>
             {allProducts.map((product, i) => (
-              <tr key={i}>
-                <td>
-                  <img src={product.image} alt="" />
+              <tr key={i} className='border-b border-slate-900/20 text-gray-20 p-6 medium-14'>
+                <td className='flexStart sm:flexCenter'>
+                  <img src={product.image} alt="" height={43} width={43} className='rounded-lg ring-1 ring-slate-900/5 my-1'/>
                 </td>
-                <td><div>{product.name}</div></td>
+                <td><div className='line-clamp-3'>{product.name}</div></td>
                 <td>${product.old_price}</td>
                 <td>${product.new_price}</td>
                 <td>{product.category}</td>
-                <td><div><TbTrash/></div></td>
+                <td><div className='bold-22 pl-6 sm:pl-14'><TbTrash/></div></td>
               </tr>
             ))}
           </tbody>
