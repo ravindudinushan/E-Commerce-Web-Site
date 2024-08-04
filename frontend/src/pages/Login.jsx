eeb4e2d7001d76cfe7c63394147acf69f7b6caf5
Ravindu Dinushan <ravindudinushan9966@gmail.com>
2024-08-04 14:49:23 +0530

    login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,17 +4,27 @@ const Login = () => {
 
   const [ state, setState ] = useState("Login");
 
+  const login = async () => {
+
+  }
+
+  const signup = async () => {
+
+  }
+
   return (
     <section className='max_padd_container flexCenter flex-col pt-32'>
       <div className='max-w-[555px] h-[600px] bg-white m-auto px-14 py-10'>
         <h3 className='h3'>{state}</h3>
         <div className='flex flex-col gap-4 mt-7'>
-          <input type="text" placeholder='Your Name' className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>
+          {state==="Sign Up"?<input name='username' type="text" placeholder='Your Name' className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>:''}
           <input type="email" placeholder='Email Address' className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>
           <input type="password" placeholder='Password' className='h-14 w-full pl-5 bg-slate-900/5 outline-none rounded-xl'/>
         </div>
         <button className='btn_dark_rounded my-5 w-full !rounded-md'>Continue</button>
-        <p className='text-black font-bold'>Already have an account? <span className='text-secondary underline cursor-pointer'>Login</span></p>
+        {state==="Sign Up"?<p className='text-black font-bold'>Already have an account? <span onClick={()=> {setState("Login")}} className='text-secondary underline cursor-pointer'>Login</span></p>
+         : 
+        <p className='text-black font-bold'>Create an account? <span onClick={()=> {setState("Sign Up")}} className='text-secondary underline cursor-pointer'>Click here</span></p>}
         <div className='flexCenter mt-6 gap-3'>
           <input type="checkbox" name='' id=''/>
           <p>By continuing, i agree to the terms of use and privacy policy.</p>
