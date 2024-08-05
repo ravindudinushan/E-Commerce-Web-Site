f2a0253a4240930bb50cb9c94adaef2691ed29ce
Ravindu Dinushan <ravindudinushan9966@gmail.com>
2024-08-05 07:48:39 +0530

    popular product

---
+++ command.sh
@@ -0,0 +1,3 @@
+node index.js - Start backend server
+npm start - start frontend server and admin server
+npm install - Install dependencies
--- frontend/src/components/Popular.jsx
+++ frontend/src/components/Popular.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
-import POPULAR from '../assets/popular'
+import React, { useEffect, useState } from 'react'
 import Item from './Item'
 
 const Popular = () => {
+
+  const [popularProducts, setpopularProducts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/popularProducts").then((response) => response.json()).then((data) => setpopularProducts(data));
+  }, [])
+  
+
   return (
     <section className='bg-primary'>
         <div className='max_padd_container py-12 xl:py-28 xl:w-[88%]'>
@@ -10,7 +17,7 @@ const Popular = () => {
             <hr className='h-[3px] md:w-1/2 mx-auto bg-gradient-to-r from-transparent via-black to-transparent mb-16'/>
             {/* container */}
             <div className='grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6'>
-                {POPULAR.map((item) => (
+                {popularProducts.map((item) => (
                     <Item key={item.id} id={item.id} image={item.image} name={item.name} new_price={item.new_price} old_price={item.old_price}/>
                 ))}
             </div>
